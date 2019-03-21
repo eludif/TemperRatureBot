c26ad662faeee0979b781de2356ea249558fbc45
faeludire <faeludire@gmail.com>
2019-03-21 21:39:35 +0000

    Initial commit

---
+++ server.py
@@ -0,0 +1,28 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import ast
+from io import BytesIO
+
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Hello, world!')
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+        r = body.decode('ascii')
+        r = ast.literal_eval(r)
+        temp = r.get("temp")
+        print(temp)
+        self.send_response(200)
+        self.end_headers()
+        response = BytesIO()
+        response.write(body)
+        self.wfile.write(response.getvalue())
+
+
+httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
+httpd.serve_forever()
---
+++ twitter_bot.py
@@ -0,0 +1,23 @@
+import time
+import tweepy
+from credentials import *
+
+# Access and authorize our Twitter credentials from credentials.py
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+
+def tweet_lesson(tweet_lines):
+
+    print(tweet_lines[0])
+    last_tweet = api.update_status(tweet_lines[0])
+
+    # create a loop to iterate over lines
+    for tweet_line in tweet_lines[1:]:
+        try:
+            print(tweet_line)
+            last_tweet = api.update_status(tweet_line, last_tweet.id)
+        except tweepy.TweepError as e:
+            print(e.reason)
+        time.sleep(5)
